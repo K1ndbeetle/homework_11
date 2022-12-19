efef3c010cefd3c9022b78194995b9a02f7aba99
K1ndbeetle <alx-s@bk.ru>
2022-12-19 23:46:12 +0300

    Homework is done

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,14 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Author author1 = new Author("Эрих Мария", "Ремарк");
+        Author author2 = new Author("Оскар", "Уайльд");
+
+        Book book1 = new Book("Жизнь взаймы", author1, 1977);
+        Book book2 = new Book("Портрет Дориана Грея", author2, 1890);
+
+        book1.setPublicationYear(1978);
+
+        System.out.println(book1.getTitle() + ", " + book1.getPublicationYear() + " год, " + author1.getFirstName() + " "  + author1.getLastName() + ".");
+        System.out.println(book2.getTitle() + ", " + book2.getPublicationYear() + " год, " + author2.getFirstName() + " "  + author2.getLastName() + ".");
     }
 }
